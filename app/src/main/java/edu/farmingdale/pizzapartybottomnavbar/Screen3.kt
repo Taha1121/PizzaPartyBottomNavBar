287537d34ab91d5563d36cada5912639a8587f9c
Taha Imran <tahaimran283@yahoo.com>
2024-10-22 14:05:08 -0400

    To DO 3 done

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
@@ -34,10 +34,11 @@ import androidx.compose.ui.unit.sp
 fun Screen3() {
     var sliderValue by remember { mutableStateOf(0.5f) }
     var chkd by remember { mutableStateOf(true) }
-    var brushColor = arrayOf(0.0f to Color.White, 0.2f to Color.LightGray, 1f to Color.White)
+    val brushColor = arrayOf(0.0f to Color.White, 0.2f to Color.Green, 1f to Color.White)
 
     val context = LocalContext.current
-    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize(),
+    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize()
+        .background(Brush.verticalGradient(colorStops = brushColor)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
         Slider(value = sliderValue, onValueChange = { sliderValue=it }, Modifier.fillMaxWidth()
